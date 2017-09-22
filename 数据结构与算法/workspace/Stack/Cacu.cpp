7a76a556ee9d05452f2eee29f1edfd6f3c36b8ce
richsoap <1500012956@pku.edu.cn>
2017-09-22 09:10:38 +0800

    finish the caculator

--- 数据结构与算法/workspace/Stack/Cacu.cpp
+++ 数据结构与算法/workspace/Stack/Cacu.cpp
@@ -3,37 +3,59 @@ int priority[256];
 int numbers[1024];
 char operate[1024];
 int operateLoc,numberLoc;
+void cacu(int level) {
+  int A,B;
+  while(operateLoc && priority[(int)operate[--operateLoc]] > level) {
+    A = numbers[--numberLoc];
+    B = numbers[--numberLoc];
+    switch(operate[operateLoc]) {
+      case '+':numbers[numberLoc ++] = A + B;
+      break;
+      case '-':numbers[numberLoc ++] = A - B;
+      break;
+      case '*':numbers[numberLoc ++] = A * B;
+      break;
+      case '/':numbers[numberLoc ++] = A / B;
+      break;
+    }
+  }
+}
 int main() {
   char str[1025];
   scanf("%s", str);
   int i = -1;
-  int intBuffer = 0;
+  int intBuffer = -1;
   operateLoc = 0;
   numberLoc = 0;
   priority['+'] = priority['-'] = 1;
   priority['*'] = priority['/'] = 2;
-  priority[')'] = priority['('] = -1;
+  priority[')'] = -1;
+  priority['('] = -2;
   while(str[++i] != '\0') {
-    switch(priority[str[i]]) {
-      case 2:operate[operateLoc ++] = str[i];
+    if(priority[(int)str[i]] == 0) {
+      intBuffer=intBuffer<0?str[i]-'0':intBuffer*10+str[i]-'0';
+    }
+    else if(intBuffer >= 0) {
+      numbers[numberLoc ++] = intBuffer;
+      intBuffer = -1;
+    }
+    switch (priority[(int)str[i]]) {
+      case 1: cacu(1);
+      operate[operateLoc ++] = str[i];
       break;
-      case 1:
-      if(priority[operate[operateLoc]] > priority[str[i]]) {
-        cacu();
-      }
-      else {
-        operate[operateLoc ++] = str[i];
-      }
       case -1:
-      if(str[i] == '(') {
-        operate[operateLoc ++] = str[i];
-      }
-      else {
-        cacu();
-        operateLoc --;
-      }
+      cacu(0);
+      break;
+      case 0:
+      break;
       default:
-      
+      operate[operateLoc ++] = str[i];
     }
   }
+  if(intBuffer >= 0) {
+    numbers[numberLoc ++] = intBuffer;
+  }
+  cacu(0);
+  printf("%d",numbers[0]);
+  return 0;
 }
