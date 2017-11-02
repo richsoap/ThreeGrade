5876977987c87b2f43dc4c0e314e2925c4059169
richsoap <1500012956@pku.edu.cn>
2017-11-02 15:09:39 +0800

    T

--- 数据结构与算法/workspace/BTree/0503.cpp
+++ 数据结构与算法/workspace/BTree/0503.cpp
@@ -1,6 +1,5 @@
 #include <algorithm>
 #include <stdio.h>
-#include <list>
 using namespace std;
 class node{
   public:
@@ -17,16 +16,53 @@ bool cmp(const node& n1,const node& n2) {
   return n1.x < n2.x;
 }
 int main() {
-  int maxBrightness,n,w,h,x,y,c;
-  list<node> queue;
-  node* buffer = new node[10000];
+  int maxBrightness,tempBrightness,n,w,h,x,y,c,subN;
+  int pointFro,pointBeh,subPointFro,subPointBeh;
+  node* buffer = new node[10001];
+  node* subBuffer = new node[10001];
   while(scanf("%d %d %d",&n,&w,&h) != EOF) {
     maxBrightness = 0;
     for(int i = 0;i < n;i ++) {
       scanf("%d %d %d",&x,&y,&c);
       buffer[i] = node(x,y,c);
     }
-    sort(buffer,buffer + n,cmp);
-
+    sort(buffer,buffer + n,cmp);//read and sort data
+    for(pointFro = pointBeh = 0;pointBeh <= n;) {
+      printf("out\n");
+      while(pointBeh <= n && buffer[pointBeh].x - buffer[pointFro].x < w) {
+        pointBeh ++;
+        printf("move right\n");
+      }//move right point to the limit location
+      subN = pointBeh - pointFro;
+      tempBrightness = 0;
+      for(int i = 0;i < subN;i ++) {
+        subBuffer[i] = buffer[i + pointFro];
+        tempBrightness += buffer[i].c;
+      }
+      if(tempBrightness <= maxBrightness) {
+        continue;
+      }//if all of the brightness is less than maxBrightness,continue
+      sort(subBuffer,subBuffer + subN);
+      tempBrightness = 0;
+      for(subPointFro = subPointBeh = 0;subPointBeh <= subN;) {
+        printf("in\n");
+        while(subPointBeh <= subN && subBuffer[subPointBeh].y - subBuffer[subPointFro].y < h) {
+          subPointBeh ++;
+          printf("move subright\n");
+          tempBrightness += subBuffer[subPointBeh].c;
+        }
+        maxBrightness = tempBrightness>maxBrightness?tempBrightness:maxBrightness;
+        while(subBuffer[subPointBeh].y - subBuffer[subPointFro].y >= h) {
+          tempBrightness -= subBuffer[subPointFro].c;
+          subPointFro ++;
+          printf("move subleft\n");
+        }
+      }
+      while(buffer[pointBeh].x - buffer[pointFro].x >= w) {
+        pointFro ++;
+        printf("move left\n");
+      }
+    }
+    printf("%d\n",maxBrightness);
   }
 }
