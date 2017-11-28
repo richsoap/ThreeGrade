cc84d018d71b2f3227f2501534e352bc7d1a9dbe
richsoap <1500012956@pku.edu.cn>
2017-11-28 19:49:49 +0800

    poj2524.cpp

--- 数据结构与算法/workspace/BTree/0503.cpp
+++ 数据结构与算法/workspace/BTree/0503.cpp
@@ -3,12 +3,16 @@
 using namespace std;
 class node{
   public:
-    int x,y,c;
+    long int x,y,c;
 
   public:
-  node(int _x = 0,int _y = 0,int _c = 0):x(_x),y(_y),c(_c) {}
+  node(long int _x = 0,long int _y = 0,long int _c = 0):x(_x),y(_y),c(_c) {}
   bool operator < (const node& n) {
-    return y < n.y;
+    if(y == n.y) {
+      return c < n.c;
+    }
+    else
+      return y < n.y;
   }
 };
 
@@ -17,50 +21,34 @@ bool cmp(const node& n1,const node& n2) {
 }
 int main() {
   int maxBrightness,tempBrightness,n,w,h,x,y,c,subN;
-  int pointFro,pointBeh,subPointFro,subPointBeh;
-  node* buffer = new node[10001];
-  node* subBuffer = new node[10001];
+  int pointFro,pointBeh,subPoint,p,q;
+  node* buffer = new node[10010];
+  node* subBuffer = new node[20010];
   while(scanf("%d %d %d",&n,&w,&h) != EOF) {
     maxBrightness = 0;
     for(int i = 0;i < n;i ++) {
-      scanf("%d %d %d",&x,&y,&c);
-      buffer[i] = node(x,y,c);
+      scanf("%ld %ld %ld",&(buffer[i].x),&(buffer[i].y),&(buffer[i].c));
     }
     sort(buffer,buffer + n,cmp);//read and sort data
-    for(pointFro = pointBeh = 0;pointBeh <= n;) {
-      printf("out\n");
-      while(pointBeh <= n && buffer[pointBeh].x - buffer[pointFro].x < w) {
+    for(pointFro = pointBeh = 0;pointBeh < n;) {
+      while(pointBeh < n && buffer[pointBeh].x - buffer[pointFro].x < w) {
         pointBeh ++;
-        printf("move right\n");
       }//move right point to the limit location
       subN = pointBeh - pointFro;
       tempBrightness = 0;
-      for(int i = 0;i < subN;i ++) {
-        subBuffer[i] = buffer[i + pointFro];
-        tempBrightness += buffer[i].c;
+      for(int i = 0,p = pointFro,q=subN;i < subN;i ++,p++,q++) {
+        subBuffer[i] = buffer[p];
+        subBuffer[q].y = subBuffer[i].y + h;
+        subBuffer[q].c = -subBuffer[i].c;
       }
-      if(tempBrightness <= maxBrightness) {
-        continue;
-      }//if all of the brightness is less than maxBrightness,continue
-      sort(subBuffer,subBuffer + subN);
+      sort(subBuffer,subBuffer + 2*subN);
       tempBrightness = 0;
-      for(subPointFro = subPointBeh = 0;subPointBeh <= subN;) {
-        printf("in\n");
-        while(subPointBeh <= subN && subBuffer[subPointBeh].y - subBuffer[subPointFro].y < h) {
-          subPointBeh ++;
-          printf("move subright\n");
-          tempBrightness += subBuffer[subPointBeh].c;
-        }
+      for(subPoint = 0;subPoint < 2 * subN;subPoint ++) {
+        tempBrightness += subBuffer[subPoint].c;
         maxBrightness = tempBrightness>maxBrightness?tempBrightness:maxBrightness;
-        while(subBuffer[subPointBeh].y - subBuffer[subPointFro].y >= h) {
-          tempBrightness -= subBuffer[subPointFro].c;
-          subPointFro ++;
-          printf("move subleft\n");
-        }
       }
       while(buffer[pointBeh].x - buffer[pointFro].x >= w) {
         pointFro ++;
-        printf("move left\n");
       }
     }
     printf("%d\n",maxBrightness);
---
+++ 数据结构与算法/workspace/Graph/poj2524.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int tree[50010];
+
+int getRoot(int j) {
+	if(0 == tree[j])
+	  return j;
+	tree[j] = getRoot(tree[j]);
+	return tree[j];
+}
+
+bool isSameParent(int j,int k) {
+	j = getRoot(j);
+	k = getRoot(k);
+	if(j == k)
+	  return true;
+	else {
+	  tree[j] = k;
+	  return false;
+	}
+}
+
+int main() {
+	int m,n,count,j,k,cases;
+	cases = 1;
+	while(true) {
+		scanf("%d %d",&n,&m);
+		if(m == 0 && n == 0) {
+			return 0;
+		}
+		for(int i = 1;i <= n;i ++) {
+			tree[i] = 0;
+		}
+		count = 0;
+		while(m --) {
+			scanf("%d %d",&j,&k);
+			printf("read\n");
+			if(!isSameParent(j,k))
+			  count ++;
+		}
+		printf("Case %d: %d\n",cases ++,n - count);
+	}
+}
